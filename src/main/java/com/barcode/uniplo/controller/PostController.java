c0bbe23d4ff0af857aee958267961ae621f28158
MoseT1 <kimmose981214@gmail.com>
2025-06-25 15:04:52 +0900

    Mose contribute

--- src/main/java/com/barcode/uniplo/controller/PostController.java
+++ src/main/java/com/barcode/uniplo/controller/PostController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.beans.PropertyEditorSupport;
@@ -56,8 +53,8 @@ public class PostController {
         return "post/postList";
     }
 
-    @GetMapping("/read")
-    public String read(Integer post_id, HttpSession session, HttpServletResponse response, Model m) throws IOException {
+    @GetMapping("/{post_id}")
+    public String read(@PathVariable("post_id") Integer post_id, HttpSession session, HttpServletResponse response, Model m) throws IOException {
         PostDto postDto = postService.read(post_id);
         UserDto loginUser = (UserDto) session.getAttribute("authUser");
 
