6a9288ebf33fc5fb53ae4e036ec7c7cf533ddbeb
chador <newspark00@gmail.com>
2025-06-26 18:34:25 +0900

    25.06.26_18:34_cart+order

--- src/main/java/com/barcode/uniplo/dao/ItemDao.java
+++ src/main/java/com/barcode/uniplo/dao/ItemDao.java
@@ -6,13 +6,10 @@ import java.util.List;
 
 public interface ItemDao {
     void insertItem(ItemDto item);
+
     List<ItemDto> selectAllItems();
+
     ItemDto selectItemById(int id);
-
-
-    // 추가
-    // 아이템 단품 가격 조회
-    int getPriceByItemId(int id);
 
 
     // 추가
